089848e84b2220fe3a6279a08927fd57f3c5b883
augustocosta79 <amcneto@hotmail.com>
2022-09-19 11:41:21 -0300

    semantic improved new mwthod getQuestionById

--- routes/quizz.ts
+++ routes/quizz.ts
@@ -1,22 +1,20 @@
 import { Router } from 'express'
 import { Quizz } from '../controllers/quizz'
 
-
 export const quizzRouter = Router()
 
 
 // GET ALL QUESTIONS FROM QUIZZ /all
-quizzRouter.get('/all', Quizz.getQuestions)
-
+quizzRouter.get('/questions', Quizz.getQuestions)
 
 // GET QUIZZ QUESTIONS FROM SPECIFIC CATEGORY /:category
 quizzRouter.get('/:category', Quizz.getQuestions)
 
 // CREATE NEW QUIZZ QUESTION
-quizzRouter.post('/newquestion', Quizz.createQuestion)
+quizzRouter.post('/create', Quizz.createQuestion)
 
 //DELETE QUESTION
-quizzRouter.delete('/question', Quizz.deteleQuestion)
+quizzRouter.delete('/delete/:id', Quizz.getQuestionById, Quizz.deteleQuestion)
 
 // UPDATE QUESTION
-quizzRouter.patch('/update', Quizz.updateQuestion)
+quizzRouter.patch('/update/:id', Quizz.getQuestionById, Quizz.updateQuestion)
