5c8b688ca7514c050f4c976c7d1d077d9425118c
augustocosta79 <amcneto@hotmail.com>
2022-09-20 11:47:04 -0300

    Add complete validation to mongoose schema

--- models/quizz.ts
+++ models/quizz.ts
@@ -1,37 +1,46 @@
 import mongoose from "mongoose";
 import { Document } from 'mongoose'
 
+interface Answer { text: string, correct: boolean, _id: number }
+
 export interface iQuestion extends Document {
     question: string,
     category: string,
-    answers: [
-        {text: string, correct: boolean},
-        {text: string, correct: boolean},
-        {text: string, correct: boolean},
-        {text: string, correct: boolean}
-    ]
-}
+    answers: Answer[]
 
-const answer = new mongoose.Schema({
-    text: {type: String, required: true},
-    correct: {type: Boolean, required: true},
-})
+}
 
 export const quizzSchema = new mongoose.Schema<iQuestion>({
     question: {
         type: String,
-        required: true
+        required: [true, 'Question  is required'],
+        validate: {
+            validator: (v: string) => { return v.length > 3},
+            message: props => 'Question must have 3 characters minimum'
+        },
     },
 
     category: {
-        required: true,
         type: String,
+        required: [true, 'Category is required'],
     },
 
     answers: {
-        required: true,
-        type: [answer, answer, answer, answer]
+        type: [
+            { 
+                text: {type: String, required: true},
+                correct: {type: Boolean, required: true},
+                _id: {select: false}
+            }
+            
+        ],
+        required: [true, 'Answers is required'],
     }
-})
+}, { versionKey: false })
+
+quizzSchema.path('answers').validate(
+    (answer: Answer[]) => { return answer.length >= 4 },
+    'At least 4 answers required'
+)
 
 export const quizzModel = mongoose.model<iQuestion>('Question', quizzSchema)
